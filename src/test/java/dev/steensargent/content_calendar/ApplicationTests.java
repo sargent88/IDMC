6e16889de43e5e14eb956da623e83fe9485624df
Steen G Sargent <ssargent@nerdunited.com>
2025-01-23 15:04:03 -0700

    first commit

---
+++ src/test/java/dev/steensargent/content_calendar/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.steensargent.content_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
