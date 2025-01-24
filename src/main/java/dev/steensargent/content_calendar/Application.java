c75940e7e7948bdca361f5aace0593cf3d8f3ce9
Steen G Sargent <ssargent@nerdunited.com>
2025-01-24 13:13:25 -0700

    lunch break commit

--- src/main/java/dev/steensargent/content_calendar/Application.java
+++ src/main/java/dev/steensargent/content_calendar/Application.java
@@ -2,12 +2,16 @@ package dev.steensargent.content_calendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		RestTemplate restTemplate = (RestTemplate) context.getBean("restTemplate");
+		System.out.println(restTemplate);
 	}
 
 }
